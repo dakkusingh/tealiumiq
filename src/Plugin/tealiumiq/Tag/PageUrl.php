f546d2194ab51e8a7668d51fac7721a70c07509c
dakku <dakku@97634.no-reply.drupal.org>
2018-09-07 20:23:29 +0100

    PageUrl tag field.

---
+++ src/Plugin/tealiumiq/Tag/PageUrl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\tealiumiq\Plugin\tealiumiq\Tag;
+
+use Drupal\tealiumiq\TealiumiqTagBase;
+
+/**
+ * The basic "Page Url" tealiumiq tag.
+ *
+ * @TealiumiqTag(
+ *   id = "page_url",
+ *   label = @Translation("Page Url"),
+ *   description = @Translation("Page Url."),
+ *   name = "page_url",
+ *   group = "page",
+ *   weight = 4,
+ *   type = "label",
+ *   secure = FALSE,
+ *   multiple = FALSE
+ * )
+ */
+class PageUrl extends TealiumiqTagBase {
+  // Nothing here yet. Just a placeholder class for a plugin.
+}
