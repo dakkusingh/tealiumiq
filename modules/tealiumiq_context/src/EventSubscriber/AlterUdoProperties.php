8d726af7ec40d0b0c4b4f7f988cf82c86999bd66
dakku <dakku@97634.no-reply.drupal.org>
2018-09-16 03:21:46 +0100

    Issue #2998970: Create an addon module to provide integration with Context module

---
+++ modules/tealiumiq_context/src/EventSubscriber/AlterUdoProperties.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\tealiumiq_context\EventSubscriber;
+
+use Drupal\context\ContextManager;
+use Drupal\tealiumiq\Event\AlterUdoPropertiesEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class AlterUdoProperties implements EventSubscriberInterface {
+
+  /**
+   * ContextManager.
+   *
+   * @var \Drupal\context\ContextManager
+   */
+  private $contextManager;
+
+  /**
+   * ContextManager constructor.
+   */
+  public function __construct(ContextManager $contextManager) {
+    $this->contextManager = $contextManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[AlterUdoPropertiesEvent::UDO_ALTER_PROPERTIES] = 'doAlterUdoProperties';
+    return $events;
+  }
+
+  /**
+   * Alter the UDO.
+   *
+   * @param \Drupal\tealiumiq\Event\AlterUdoPropertiesEvent $event
+   *   Alter Udo Properties Event.
+   */
+  public function doAlterUdoProperties(AlterUdoPropertiesEvent $event) {
+    foreach ($this->contextManager->getActiveReactions('tealiumiq_context') as $reaction) {
+      $tealiumiqContext = array_filter($reaction->execute());
+
+      // Unset the ID.
+      unset($tealiumiqContext['id']);
+
+      if (!empty($tealiumiqContext)) {
+        $event->setProperties(
+          $tealiumiqContext
+        );
+      }
+    }
+  }
+
+}
